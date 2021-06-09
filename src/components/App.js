7d8c538556c11514429fb7f33a2b3cba36e6f193
Angel <53478143+007sya@users.noreply.github.com>
2021-06-09 13:53:11 +0200

    Almost finished functinality

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import IntegerInput from "./IntegerInput";
+import CalculationResult from "./CalculationResult";
+import { useState } from "react";
+import { Fibonacci } from "../utils";
 
 function App() {
-  return <div className="App-container"></div>;
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState(undefined);
+
+  async function handleCalculateButtonClick(_) {
+    // we make the function async to prevent
+    // freezing the UI
+    setResult(Fibonacci(inputValue));
+  }
+  return (
+    <div className="App-container column">
+      <div className="column horizontal-center">
+        <h1>Fibonacci Calculator</h1>
+        <IntegerInput
+          value={inputValue}
+          setValue={setInputValue}
+          setResult={setResult}
+        />
+        <button
+          onClick={handleCalculateButtonClick}
+          style={{ maxWidth: 100, marginTop: 10 }}
+        >
+          Calculate Fibonacci
+        </button>
+      </div>
+      <CalculationResult result={result} />
+    </div>
+  );
 }
 
 export default App;
