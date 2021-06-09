2183418cca7ff603d80c38896c6dfff3f3b95ce8
Angel <53478143+007sya@users.noreply.github.com>
2021-06-09 13:55:40 +0200

    Added Fibonacci functions (Recursive and Iterative)

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,33 @@ export function stringIsNaturalNumber(string) {
   //  start - 0 or 1-9 - [0,9] {0,} - end
   return /^(0|[1-9]\d*)$/.test(string);
 }
+
+// Fibonacci(0) == 0
+// Fibonacci(1) == 1
+// here we will store the Fibonacci results to
+// prevent already made calculations
+let fibonacciCache = [0, 1, 1];
+
+export function FibonacciRecursive(naturalNumber) {
+  // memoized version of Fibonacci sucesion
+  // for big numbers fails because stack size is exceeded
+  if (fibonacciCache[naturalNumber]) {
+    return fibonacciCache[naturalNumber];
+  }
+  fibonacciCache[naturalNumber] =
+    FibonacciRecursive(naturalNumber - 1) +
+    FibonacciRecursive(naturalNumber - 2);
+  return fibonacciCache[naturalNumber];
+}
+
+export function Fibonacci(naturalNumber) {
+  // iterative version of Fibonacci with memoization
+  if (fibonacciCache[naturalNumber]) {
+    return fibonacciCache[naturalNumber];
+  }
+  const N = fibonacciCache.length;
+  for (let i = N; i <= naturalNumber; i++) {
+    fibonacciCache[i] = fibonacciCache[i - 1] + fibonacciCache[i - 2];
+  }
+  return fibonacciCache[naturalNumber];
+}
