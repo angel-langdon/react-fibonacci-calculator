7aebbcdd1957819afa0be65e5baf098ee21e986a
Angel <53478143+007sya@users.noreply.github.com>
2021-06-09 12:36:05 +0200

    Added utils module

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export function stringIsNaturalNumber(string) {
+  // regex to match positive integer numbers
+  //  start - 0 or 1-9 - [0,9] {0,} - end
+  console.log(string);
+  return /^(0|[1-9]\d*)$/.test(string);
+}
