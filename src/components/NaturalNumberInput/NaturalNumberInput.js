2f693479afa2659ec9e41803ca8a06ccf6123eb4
Angel <53478143+007sya@users.noreply.github.com>
2021-06-10 03:13:28 +0200

    Renaming

---
+++ src/components/NaturalNumberInput/NaturalNumberInput.js
@@ -0,0 +1,31 @@
+/* global BigInt */
+import React from "react";
+import { stringIsNaturalNumber } from "utils/string-utils";
+import "./NaturalNumberInput.css";
+
+export default function IntegerInput(props) {
+  function processInput(event) {
+    const value = event.target.value;
+    // in case the value is an empty string
+    // set it and do nothing
+    if (value === "") {
+      props.setValue(value);
+      props.setResult(undefined);
+      return;
+    }
+    if (!stringIsNaturalNumber(value)) return;
+    const integer = parseInt(value);
+    props.setValue(integer);
+    props.setResult(undefined);
+  }
+
+  return (
+    <input
+      className="natural-integer-input"
+      value={props.value.toLocaleString("fullwide", { useGrouping: false })} // fullwide to prevent scientific notation
+      onChange={processInput}
+      aria-label="integer-input"
+      placeholder="Specify a natural number"
+    ></input>
+  );
+}
