f0e8ba915e8b7f7dcfff7945a5451504eede6059
Angel <53478143+007sya@users.noreply.github.com>
2021-06-10 03:35:21 +0200

    Added two tests

--- src/components/CalculationResult/CalculationResult.test.js
+++ src/components/CalculationResult/CalculationResult.test.js
@@ -1,11 +1,22 @@
-import { fireEvent, render, shallow } from "@testing-library/react";
+import React from "react";
+import TestRenderer from "react-test-renderer";
 import CalculationResult from "./CalculationResult";
 
 describe("CalculationResult", () => {
   it("should render the results", () => {
     const int = BigInt(12312323123123);
-    const comp = render(<CalculationResult result={int} />);
-    console.log(comp.toJSON());
-    expect(comp.text().includes(int.toLocaleString()).toBe(true));
+    const testInstance = TestRenderer.create(
+      <CalculationResult result={int} />
+    ).root;
+    const content = testInstance.findByProps({ className: "content" });
+    expect(content.children.includes(int.toLocaleString())).toBe(true);
   });
+});
+
+it("should loader if isLoading is true", () => {
+  const int = BigInt(12312323123123);
+  const testInstance = TestRenderer.create(
+    <CalculationResult result={int} isLoading={true} />
+  ).root;
+  const content = testInstance.findByProps({ className: "loader" });
 });
