725882083f1bfc6965b5cc5d0803ba9a99a7500e
Angel <53478143+007sya@users.noreply.github.com>
2021-06-11 01:41:37 +0200

    Finished tests implementation for string utils

--- src/utils/string-utils.test.js
+++ src/utils/string-utils.test.js
@@ -1,12 +1,15 @@
 import { stringIsNaturalNumber } from "./string-utils";
 
-test("It correctly checks if a string is a natural number", () => {
-  expect(stringIsNaturalNumber("hello world")).toBe(false);
-  expect(stringIsNaturalNumber("123123hello world")).toBe(false);
-  expect(stringIsNaturalNumber("-123123")).toBe(false);
-  expect(stringIsNaturalNumber("123  123")).toBe(false);
-  expect(stringIsNaturalNumber("123\n")).toBe(false);
-
-  expect(stringIsNaturalNumber("123123")).toBe(true);
-  expect(stringIsNaturalNumber("0")).toBe(true);
+describe("string-utils", () => {
+  it("stringIsNaturalNumber - It correctly checks if a string is a natural number", () => {
+    expect(stringIsNaturalNumber("hello world")).toBe(false);
+    expect(stringIsNaturalNumber("123123hello world")).toBe(false);
+    expect(stringIsNaturalNumber("-123123")).toBe(false);
+    expect(stringIsNaturalNumber("123  123")).toBe(false);
+    expect(stringIsNaturalNumber("123\n")).toBe(false);
+    expect(stringIsNaturalNumber("123123")).toBe(true);
+    expect(stringIsNaturalNumber("0")).toBe(true);
+    expect(stringIsNaturalNumber("0000000000")).toBe(false);
+    expect(stringIsNaturalNumber("12311231231321323122323")).toBe(true);
+  });
 });
