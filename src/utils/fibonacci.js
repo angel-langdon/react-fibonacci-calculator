f66a1a03953bec0a1309b30c457fec60917cd558
Angel <53478143+007sya@users.noreply.github.com>
2021-06-11 01:40:50 +0200

    Finished implementation of fibonacci

--- src/utils/fibonacci.js
+++ src/utils/fibonacci.js
@@ -1,66 +1,64 @@
-// We need to enable JavaScript Built In BigInt
+// We need to enable JavaScript Built-in BigInt
 /* global BigInt */
-// Fibonacci(0) == 0
-// Fibonacci(1) == 1
-// Fibonacci(2) == 1
-// here we will store the Fibonacci results to
-// prevent already made calculations
 
 // this number can be tweaked to achieve more efficient calculations
 // but should be carefully selected to prevent filling JS maximum heap size
-const MAXIMUM_ARRAY_SIZE_FOR_MEMOIZATION = 10_000;
+export const MAXIMUM_ARRAY_LENGTH_FOR_MEMOIZATION = 10_000;
 // we use BigInt for bigger precision
-let fibonacciCache = [BigInt(0), BigInt(1), BigInt(1)];
+export const initialCache = [BigInt(0), BigInt(1), BigInt(1)];
+// we make a copy to prevent modifying the original cache
+export let fibonacciCache = [...initialCache];
 
-export function FibonacciRecursive(naturalNumber) {
-  // memoized version of Fibonacci succession
-  // for big numbers fails because stack size is exceeded
-  if (fibonacciCache[naturalNumber]) {
-    return fibonacciCache[naturalNumber];
-  }
-  fibonacciCache[naturalNumber] =
-    FibonacciRecursive(naturalNumber - 1) +
-    FibonacciRecursive(naturalNumber - 2);
-  return fibonacciCache[naturalNumber];
+export function cleanFibonacciCache() {
+  fibonacciCache = [...initialCache];
 }
+
+//export function FibonacciRecursive(naturalNumber) {
+//  // memoized version of Fibonacci succession
+//  // for big numbers fails because stack size is exceeded
+//  if (fibonacciCache[naturalNumber]) {
+//    return fibonacciCache[naturalNumber];
+//  }
+//  fibonacciCache[naturalNumber] =
+//    FibonacciRecursive(naturalNumber - 1) +
+//    FibonacciRecursive(naturalNumber - 2);
+//  return fibonacciCache[naturalNumber];
+//}
 
 export function FibonacciIterativeMemoized(naturalNumber) {
   // iterative version of Fibonacci with memoization
-  if (fibonacciCache[naturalNumber]) {
+  if (!(fibonacciCache[naturalNumber] === undefined)) {
     return fibonacciCache[naturalNumber];
   }
   const N = fibonacciCache.length;
   for (let i = N; i <= naturalNumber; i++) {
     const calc = fibonacciCache[i - 1] + fibonacciCache[i - 2];
     fibonacciCache[i] = calc;
-    // In case we have reached the javascript maximum precision just return Infinity
-    if (calc === Infinity) return calc;
   }
-
   return fibonacciCache[naturalNumber];
 }
 export function FibonacciIterative(naturalNumber) {
   // iterative version of Fibonacci using already done calculations
-  if (fibonacciCache[naturalNumber]) {
+  if (!(fibonacciCache[naturalNumber] === undefined)) {
     return fibonacciCache[naturalNumber];
   }
   const N = fibonacciCache.length;
-  let lastValue = fibonacciCache[fibonacciCache.length - 1];
-  let penultimateValue = fibonacciCache[fibonacciCache.length - 2];
+  let lastValue = fibonacciCache[N - 1];
+  let penultimateValue = fibonacciCache[N - 2];
   let current;
   for (let i = N; i <= naturalNumber; i++) {
     current = lastValue + penultimateValue;
     penultimateValue = lastValue;
     lastValue = current;
-    // In case we have reached the javascript maximum precision just return Infinity
-    if (current === Infinity) return current;
   }
+
   return current;
 }
 
+// wrapper for selecting Fibonacci method
 export function Fibonacci(naturalNumber) {
   // in case the natural number is small we can use memoized version
-  if (naturalNumber < MAXIMUM_ARRAY_SIZE_FOR_MEMOIZATION) {
+  if (naturalNumber < MAXIMUM_ARRAY_LENGTH_FOR_MEMOIZATION) {
     return FibonacciIterativeMemoized(naturalNumber);
   }
   // if the natural number is big we should use the pure iterative version
