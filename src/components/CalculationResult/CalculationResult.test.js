0ff96e3176afb71462affc039e060ce9f8ae89ba
Angel <53478143+007sya@users.noreply.github.com>
2021-06-11 01:38:27 +0200

    Added tests for results component

--- src/components/CalculationResult/CalculationResult.test.js
+++ src/components/CalculationResult/CalculationResult.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import TestRenderer from "react-test-renderer";
-import CalculationResult from "./CalculationResult";
+import CalculationResult, { CLASS_NAME } from "./CalculationResult";
 
 describe("CalculationResult", () => {
-  it("should render the results", () => {
+  it("should render the result if it its passed as a prop", () => {
     const int = BigInt(12312323123123);
     const testInstance = TestRenderer.create(
       <CalculationResult result={int} />
@@ -11,12 +11,22 @@ describe("CalculationResult", () => {
     const content = testInstance.findByProps({ className: "content" });
     expect(content.children.includes(int.toLocaleString())).toBe(true);
   });
-});
 
-it("should loader if isLoading is true", () => {
-  const int = BigInt(12312323123123);
-  const testInstance = TestRenderer.create(
-    <CalculationResult result={int} isLoading={true} />
-  ).root;
-  const content = testInstance.findByProps({ className: "loader" });
+  it("should render loader div if isLoading is true", () => {
+    const testInstance = TestRenderer.create(
+      <CalculationResult result={undefined} isLoading={true} />
+    ).root;
+    const content = testInstance.findByProps({ className: "loader" });
+    expect(content).toBeTruthy();
+  });
+
+  it("should render an empty div if result is undefined and isLoading is false", () => {
+    const testInstance = TestRenderer.create(
+      <CalculationResult result={undefined} isLoading={false} />
+    ).root;
+    const content = testInstance.findByProps({
+      className: "empty-div " + CLASS_NAME,
+    });
+    expect(content).toBeTruthy();
+  });
 });
